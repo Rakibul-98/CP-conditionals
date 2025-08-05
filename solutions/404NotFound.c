ccf3dffd5115ef4ba9b3b82cdcaadaa4a08d66fa
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-08-05 20:00:26 +0600

    added solution 4,5

---
+++ solutions/404NotFound.c
@@ -0,0 +1,16 @@
+// https://www.codechef.com/problems/ERROR404
+
+#include <stdio.h>
+
+int main()
+{
+    int X;
+    scanf("%d", &X);
+    
+    if(X == 404){
+        printf("NOT FOUND\n");
+    }else{
+        printf("FOUND\n");
+    }
+    return 0;
+}
---
+++ solutions/MinimumWage.c
@@ -0,0 +1,16 @@
+// https://www.codechef.com/problems/MINWAGE
+
+#include <stdio.h>
+
+int main()
+{
+    int X;
+    scanf("%d", &X);
+    
+    if(X > 11){
+        printf("YES\n");
+    }else{
+        printf("NO\n");
+    }
+    return 0;
+}

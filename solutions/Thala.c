2c335cd719872dad311463b3c08829e43698f8b1
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-08-05 19:51:19 +0600

    added solution 1, 2

---
+++ solutions/Thala.c
@@ -0,0 +1,16 @@
+// https://vjudge.net/problem/CodeChef-THALA7
+
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    
+    if(N==7){
+        printf("THALA");
+    }else{
+        printf("SADGE");
+    }
+    return 0;
+}
---
+++ solutions/smallLargeEqual.c
@@ -0,0 +1,18 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_2_A
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if(a < b){
+        printf("a < b\n");
+    }else if (a > b){
+        printf("a > b\n");
+    }else if(a == b){
+        printf("a == b\n");
+    }
+    return 0;
+}
